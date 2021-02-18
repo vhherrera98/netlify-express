b8c78dfaa000465f876a811c9da2d53bb1afcd4d
Victor Herrera <vhherrera98@gmail.com>
2021-02-18 11:33:30 -0400

    test

--- express/server.js
+++ express/server.js
@@ -11,6 +11,13 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
 });
+
+router.get('/okay', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.sed('<h1>PERRA</h1>');
+  res.end();
+});
+
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
